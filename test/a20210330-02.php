ed54dc57883e31be2c497f9ebc0d16a1d89f851f
unknown <cpg.recruithr@gmail.com>
2021-03-30 12:12:26 +0800

    3/30

---
+++ test/a20210330-02.php
@@ -0,0 +1,11 @@
+<?php
+
+$a=66;
+$b='22';
+$s='str';
+
+echo $a+$b;
+print '<br>';
+
+echo $a*$b;
+echo '<br>';
---
+++ test/a20210330-03.php
@@ -0,0 +1,11 @@
+
+<?php
+
+$name="Shin";
+
+echo"Hello $name <br>";
+echo 'Hello $name <br>';
+
+echo"Hello {$name}1232 <br>";
+echo"Hello ${name}123  <br>";
+
